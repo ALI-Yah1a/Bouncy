3b1ca360aeb4ffb31b53f66c3e5c21ab888065f9
ALI-Yah1a <aliyahia1230123@gmail.com>
2026-02-02 23:34:48 +0200

    Adding moving platforms & finishing the first area scene

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,7 +4,15 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -350.0
+var direction = 0
 
+func _input(event):
+	if event.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	if event.is_action_pressed("move_down"):
+		set_collision_mask_value(10, false)
+	else:
+		set_collision_mask_value(10, true)
 func _physics_process(delta):
 	
 	if velocity.x > 1 or velocity.x < -1:
@@ -19,14 +27,13 @@ func _physics_process(delta):
 	elif velocity.y > 0.0:
 		animated_sprite_2d.play("fall")
 	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	
 
-	var direction := Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	else:           
+		velocity.x = move_toward(velocity.x, 0, SPEED )
 
 	move_and_slide()
 	
