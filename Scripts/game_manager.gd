5a3676ee33b386c21084bee6c73b695f4916d552
ALI-Yah1a <aliyahia1230123@gmail.com>
2026-02-03 03:16:58 +0200

    creating another area scene

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,8 +1,15 @@
 extends Node
 
 var current_area = 1
+var is_returning = false
+
 func next_level():
-	current_area += 1
+	if current_area == 1:
+		current_area = 2
+		is_returning = false
+	else:
+		current_area = 1
+		is_returning = true
 	var full_path = "res://Scenes/area_" + str(current_area) + ".tscn"
 	get_tree().change_scene_to_file(full_path)
 	print("The player has entered the gate!")
--- Scripts/gate.gd
+++ Scripts/gate.gd
@@ -1,4 +1,7 @@
 extends Area2D
+class_name Gate
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 func _on_body_entered(body):
